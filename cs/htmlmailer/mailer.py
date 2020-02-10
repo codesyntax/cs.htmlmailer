89bb2b9efa037acb9b5a554942ec9060c326faea
Mikel Larreategi <mlarreategi@codesyntax.com>
2020-02-10 13:20:55 +0100

    black

--- cs/htmlmailer/mailer.py
+++ cs/htmlmailer/mailer.py
@@ -1,25 +1,35 @@
-__doc__ = ''' Mailer stolen from collective.singing '''
-
-import formatter
-import htmllib
-import StringIO
-import email 
+__doc__ = """ Mailer stolen from collective.singing """
 
 from email.Header import Header
 from email.MIMEMultipart import MIMEMultipart
 from email.MIMEText import MIMEText
 from email.Utils import formatdate
-
 from types import UnicodeType
+
+import email
+import formatter
+import htmllib
+import StringIO
+
 
 class HTMLMustBeUnicodeException(Exception):
     pass
 
+
 class TextMustBeUnicodeException(Exception):
     pass
 
-def create_html_mail(subject, html, text=None, from_addr=None, to_addr=None,
-                     cc_addrs=[], headers=None, encoding='UTF-8'):
+
+def create_html_mail(
+    subject,
+    html,
+    text=None,
+    from_addr=None,
+    to_addr=None,
+    cc_addrs=[],
+    headers=None,
+    encoding="UTF-8",
+):
     """Create a mime-message that will render HTML in popular
     MUAs, text in better ones.
     """
@@ -30,53 +40,53 @@ def create_html_mail(subject, html, text=None, from_addr=None, to_addr=None,
     if type(html) is not UnicodeType:
         raise HTMLMustBeUnicodeException
 
-    
     html = html.encode(encoding)
     if text is None:
         # Produce an approximate textual rendering of the HTML string,
         # unless you have been given a better version as an argument
         textout = StringIO.StringIO()
-        formtext = formatter.AbstractFormatter(formatter.DumbWriter(
-                        textout, plain_text_maxcols))
+        formtext = formatter.AbstractFormatter(
+            formatter.DumbWriter(textout, plain_text_maxcols)
+        )
         parser = htmllib.HTMLParser(formtext)
         parser.feed(html)
         parser.close()
-        
+
         # append the anchorlist at the bottom of a message
-        # to keep the message readable. 
+        # to keep the message readable.
         counter = 0
-        anchorlist  = "\n\n" + ("-" * plain_text_maxcols) + "\n\n"
+        anchorlist = "\n\n" + ("-" * plain_text_maxcols) + "\n\n"
         for item in parser.anchorlist:
             counter += 1
             anchorlist += "[%d] %s\n" % (counter, item)
-        
+
         text = textout.getvalue() + anchorlist
         del textout, formtext, parser, anchorlist
     else:
         if type(text) is not UnicodeType:
             raise TextMustBeUnicodeException
-        
+
         text = text.encode(encoding)
-        
-    # if we would like to include images in future, there should 
+
+    # if we would like to include images in future, there should
     # probably be 'related' instead of 'mixed'
-    msg = MIMEMultipart('mixed')
+    msg = MIMEMultipart("mixed")
     # maybe later :)  msg['From'] = Header("%s <%s>" % (send_from_name, send_from), encoding)
-    msg['Subject'] = Header(subject, encoding)
-    msg['From'] = from_addr
-    msg['To'] = to_addr
+    msg["Subject"] = Header(subject, encoding)
+    msg["From"] = from_addr
+    msg["To"] = to_addr
     if cc_addrs:
-        msg['Cc'] = ', '.join(cc_addrs)
-    msg['Date'] = formatdate(localtime=True)
+        msg["Cc"] = ", ".join(cc_addrs)
+    msg["Date"] = formatdate(localtime=True)
     msg["Message-ID"] = email.Utils.make_msgid()
     if headers:
         for key, value in headers.items():
             msg[key] = value
-    msg.preamble = 'This is a multi-part message in MIME format.'
+    msg.preamble = "This is a multi-part message in MIME format."
 
-    alternatives = MIMEMultipart('alternative')
+    alternatives = MIMEMultipart("alternative")
     msg.attach(alternatives)
-    alternatives.attach( MIMEText(text, 'plain', _charset=encoding) )
-    alternatives.attach( MIMEText(html, 'html',  _charset=encoding) )
+    alternatives.attach(MIMEText(text, "plain", _charset=encoding))
+    alternatives.attach(MIMEText(html, "html", _charset=encoding))
 
     return msg
--- setup.py
+++ setup.py
@@ -1,32 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.1'
+version = "1.1"
 
-setup(name='cs.htmlmailer',
-      version=version,
-      description="A library to send emails with HTML and Text mixed content",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      # Get more strings from
-      # http://pypi.python.org/pypi?:action=list_classifiers
-      classifiers=[
-        "Programming Language :: Python",
-        ],
-      keywords='',
-      author='Mikel Larreategi',
-      author_email='mlarreategi@codesyntax.com',
-      url='http://code.codesyntax.com/private/cs.htmlmailer/',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['cs'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'setuptools',
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
+setup(
+    name="cs.htmlmailer",
+    version=version,
+    description="A library to send emails with HTML and Text mixed content",
+    long_description=open("README.txt").read()
+    + "\n"
+    + open(os.path.join("docs", "HISTORY.txt")).read(),
+    # Get more strings from
+    # http://pypi.python.org/pypi?:action=list_classifiers
+    classifiers=["Programming Language :: Python"],
+    keywords="",
+    author="Mikel Larreategi",
+    author_email="mlarreategi@codesyntax.com",
+    url="https://github.com/codesyntax/cs.htmlmailer/",
+    license="GPL",
+    packages=find_packages(exclude=["ez_setup"]),
+    namespace_packages=["cs"],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+        # -*- Extra requirements: -*-
+    ],
+    entry_points="""
       # -*- Entry points: -*-
       """,
-      )
+)
